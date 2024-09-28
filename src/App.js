1e2f57bc9326261d1a00f34b242d3c6b92e2b0d1
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-09-28 14:15:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import "./App.css";
 
-import Todolist from "./components/Todolist";
-import Catfact from "./components/Catfact";
-// import "./CSS/Todolist.css";
-// import "./CSS/Catfact.css";
-import Excusegen from "./components/Excusegen";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import "./CSS/Excusegen.css";
+
 import PitchDetector from "./components/PitchDetector";
 import "./CSS/PitchDetector.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Routes>
-          <Route path="/todo" element={<Todolist />}></Route>
-          <Route path="/catfact" element={<Catfact />}></Route>
-          <Route path="/excuse" element={<Excusegen />}></Route>
-          <Route path="/pitch" element={<PitchDetector />}></Route>
-        </Routes>
-      </Router> */}
-      {/* <Todolist /> */}
-      {/* <Catfact /> */}
       <PitchDetector />
     </div>
   );
